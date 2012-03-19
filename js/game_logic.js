116b6b2d237e5b592a91dc67c595c4a404326a1e
ashafein <ashafein@ya.ru>
2012-03-19 19:35:07 +0200

    pisture and random int funcrion added

--- js/game_logic.js
+++ js/game_logic.js
@@ -1,26 +1,45 @@
 $(document).ready(function() {
 	t_colls = 10;
 	t_rows = 10;
+	random_generation = true;
 
-	gen_rows(t_rows, t_colls);
+	gen_table(t_rows, t_colls, random_generation);
 });
 
-function gen_rows(t_rows, t_colls){
+
+function gen_table(t_rows, t_colls, random_generation){
+
+	life = new Array(t_rows, t_colls);
+
+	if(random_generation){
+		for(i = 0; i < t_rows; i++){
+			for(j = 0; j < t_colls; j++){
+				h = getRandomInt(0, 1) ;
+				console.log(h);
+				life[i,j] = getRandomInt(0, 1);
+			}
+		}
+	}
+
 	wrapper = document.getElementById('table_wrap');
-	table = document.createElement("table");
+	table = document.createElement('table');
+	table.setAttribute('class', 'life_field');
 	wrapper.appendChild(table);
 
-	row=null;
 	for(i = 0; i < t_rows; i++){
 		var tr = document.createElement('tr');
 		tr.setAttribute('id','row_'+i);
+
 		for(j = 0; j < t_colls; j++){
 			var td = document.createElement('td');
 			td.setAttribute('id','cell_'+i+'_'+j);
+			td.setAttribute('class', 'alive');
 			tr.appendChild(td);
 		};
 		table.appendChild(tr);
 	};
-
 };
 
+function getRandomInt(min, max){
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
