8d10f71f432d991034cbb0ce530332975750a972
ashafein <ashafein@ya.ru>
2012-03-19 20:01:33 +0200

    random function works finally!!!!

--- js/game_logic.js
+++ js/game_logic.js
@@ -9,21 +9,21 @@ $(document).ready(function() {
 
 function gen_table(t_rows, t_colls, random_generation){
 
-	life = new Array(t_rows, t_colls);
+	var life = new Array();
 
 	if(random_generation){
 		for(i = 0; i < t_rows; i++){
+			life[i] = new Array();
 			for(j = 0; j < t_colls; j++){
-				h = getRandomInt(0, 1) ;
-				console.log(h);
-				life[i,j] = getRandomInt(0, 1);
+				life[i][j] = getRandomInt(0.5);
 			}
 		}
 	}
+				console.log(life);
 
 	wrapper = document.getElementById('table_wrap');
 	table = document.createElement('table');
-	table.setAttribute('class', 'life_field');
+	table.setAttribute('class','life_field');
 	wrapper.appendChild(table);
 
 	for(i = 0; i < t_rows; i++){
@@ -33,13 +33,17 @@ function gen_table(t_rows, t_colls, random_generation){
 		for(j = 0; j < t_colls; j++){
 			var td = document.createElement('td');
 			td.setAttribute('id','cell_'+i+'_'+j);
-			td.setAttribute('class', 'alive');
+
+			if(life[i][j] == 1){
+				td.setAttribute('class','alive');
+			}else{td.setAttribute('class','dead');};
+
 			tr.appendChild(td);
-		};
+		}
 		table.appendChild(tr);
 	};
 };
 
-function getRandomInt(min, max){
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomInt(val){
+	return Math.floor(Math.random() > val);
 };
